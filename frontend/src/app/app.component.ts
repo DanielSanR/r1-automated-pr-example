ba254eb246448828ffeaff63e9f3dfb34434604c
Daniel Romero <danielromero320@gmail.com>
2025-01-28 01:38:21 -0300

    Add frontend/src/app/app.component.ts

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,16 @@
+
+import { Component } from '@angular/core';
+import { ChatService } from './services/chat.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ChatBot';
+  constructor(private chatService: ChatService) {
+    this.chatService.loadHistory();
+  }
+}
+      
