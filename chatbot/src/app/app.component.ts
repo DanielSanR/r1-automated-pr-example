adeac7345ce42b73ff53ac91d43e5351a155b5d5
Daniel Romero <danielromero320@gmail.com>
2025-01-28 01:39:50 -0300

    Add chatbot/src/app/app.component.ts

---
+++ chatbot/src/app/app.component.ts
@@ -0,0 +1,14 @@
+
+import { Component } from '@angular/core';
+import { ChatService } from './services/chat.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-chatbot';
+  constructor(private chatService: ChatService) {}
+}
+      
